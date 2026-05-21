3b292c5380b1cf8f5dff6a6588d05d63b0071236
Ankur Kumar <264759787+kumarankur-17@users.noreply.github.com>
2026-05-21 15:56:05 +0530

    Added the final code

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,15 +1,15 @@
 n=int(input())
 l=[]
-for i in range(n):
-    l.append(int(input()))
+l.extend(list(map(int, input().split())))
+
 dp=[[0]*n for i in range(n)]
 
 for i in range(n):
     dp[i][i]=l[i]
 
-for len in range(2,n+1):
-    for i in range(n-len+1):
-        j=i+len-1
+for length in range(2,n+1):
+    for i in range(n-length+1):
+        j=i+length-1
         dp[i][j]=max(l[i]-dp[i+1][j],l[j]-dp[i][j-1])
 
 if dp[0][n-1]>0:
@@ -17,4 +17,4 @@ if dp[0][n-1]>0:
 elif dp[0][n-1]<0:
     print("Player 2 wins")
 else:
-    print("Tie")
+    print("Its a draw")
